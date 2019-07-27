08c6d3beb3a121b49a78bafbdde694fd2ba39657
ka1301 <ka1301@outlook.jp>
2019-07-27 11:30:18 +0000

    フォーリンキーを外した

--- database/migrations/2019_07_11_111133_create_posts_table.php
+++ database/migrations/2019_07_11_111133_create_posts_table.php
@@ -24,7 +24,7 @@ class CreatePostsTable extends Migration
             $table->string('image')->nullable(); //ファイル名（ファイルパス）
             $table->timestamps();
             
-            $table->foreign('user_id')->references('id')->on('users');
+            // $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
--- database/migrations/2019_07_13_101948_create_likes_table.php
+++ database/migrations/2019_07_13_101948_create_likes_table.php
@@ -19,8 +19,8 @@ class CreateLikesTable extends Migration
             $table->unsignedInteger('user_id');
             $table->timestamps();
             
-            $table->foreign('post_id')->references('id')->on('posts');
-            $table->foreign('user_id')->references('id')->on('users');
+            // $table->foreign('post_id')->references('id')->on('posts');
+            // $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
